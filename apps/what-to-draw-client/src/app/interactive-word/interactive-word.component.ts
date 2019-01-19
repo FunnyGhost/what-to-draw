03f52ec236c572092b024b4dd203e0dbf193e1cb
Catalin Ciubotaru <catalinciubot@gmail.com>
2019-01-19 23:43:27 +0100

    Implemented InteractiveWord component

--- apps/what-to-draw-client/src/app/interactive-word/interactive-word.component.ts
+++ apps/what-to-draw-client/src/app/interactive-word/interactive-word.component.ts
@@ -1,12 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output
+} from '@angular/core';
 
 @Component({
   selector: 'what-to-draw-interactive-word',
   templateUrl: './interactive-word.component.html',
-  styleUrls: ['./interactive-word.component.scss']
+  styleUrls: ['./interactive-word.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class InteractiveWordComponent implements OnInit {
+  @Input() word: string;
+  @Output() refreshWord = new EventEmitter<string>();
+  @Output() removeWord = new EventEmitter<string>();
+
   constructor() {}
 
   ngOnInit() {}
+
+  onRefresh(): void {
+    this.refreshWord.emit(this.word);
+  }
+
+  onRemove(): void {
+    this.removeWord.emit(this.word);
+  }
 }
