e462f0dfb366f3d4f54838af9937a92276579a7d
Catalin Ciubotaru <catalinciubot@gmail.com>
2019-01-20 23:50:53 +0100

    Added bounce animation

--- apps/what-to-draw-client/src/app/animations.ts
+++ apps/what-to-draw-client/src/app/animations.ts
@@ -1,5 +1,6 @@
 import {
   animate,
+  keyframes,
   state,
   style,
   transition,
@@ -20,5 +21,64 @@ export let slide = trigger('slide', [
 
   transition(':leave', [
     animate(500, style({ transform: 'translateX(-100%)' }))
+  ])
+]);
+
+export let bounce = trigger('bounce', [
+  transition(':enter', [
+    animate(
+      '0.5s cubic-bezier(0.215, 0.61, 0.355, 1)',
+      keyframes([
+        style({
+          offset: 0,
+          opacity: 0,
+          transform: 'translateY(-3000px)'
+        }),
+        style({
+          offset: 0.6,
+          opacity: 1,
+          transform: 'translateY(25px)'
+        }),
+        style({
+          offset: 0.75,
+          transform: 'translateY(-10px)'
+        }),
+        style({
+          offset: 0.9,
+          transform: 'translateY(5px)'
+        }),
+        style({
+          offset: 1,
+          transform: 'translateY(0)'
+        })
+      ])
+    )
+  ]),
+
+  transition(':leave', [
+    animate(
+      '0.5s ease-in',
+      keyframes([
+        style({
+          offset: 0.2,
+          transform: 'translateY(10px)'
+        }),
+        style({
+          offset: 0.4,
+          opacity: 1,
+          transform: 'translateY(-20px)'
+        }),
+        style({
+          offset: 0.45,
+          opacity: 1,
+          transform: 'translateY(-20px)'
+        }),
+        style({
+          offset: 1,
+          opacity: 0,
+          transform: 'translateY(2000px)'
+        })
+      ])
+    )
   ])
 ]);
