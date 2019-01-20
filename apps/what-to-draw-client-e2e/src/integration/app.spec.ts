1a749c736b8688d3aa6dd3e51da9cbe3054b8d03
Catalin Ciubotaru <catalinciubot@gmail.com>
2019-01-20 16:07:01 +0100

    Removed useless e2e test

--- apps/what-to-draw-client-e2e/src/integration/app.spec.ts
+++ apps/what-to-draw-client-e2e/src/integration/app.spec.ts
@@ -1,9 +1,3 @@
-import { getGreeting } from '../support/app.po';
-
 describe('Hello Nx', () => {
   beforeEach(() => cy.visit('/'));
-
-  it('should display welcome message', () => {
-    getGreeting().contains('Welcome to what-to-draw-client!');
-  });
 });
