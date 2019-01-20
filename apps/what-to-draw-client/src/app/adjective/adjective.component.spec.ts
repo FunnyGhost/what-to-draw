1ca057b0834f99fc5f2e1535be98737d41c655ba
Catalin Ciubotaru <catalinciubot@gmail.com>
2019-01-20 16:05:16 +0100

    Fixed test

--- apps/what-to-draw-client/src/app/adjective/adjective.component.spec.ts
+++ apps/what-to-draw-client/src/app/adjective/adjective.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { InteractiveWordComponent } from '../interactive-word/interactive-word.component';
 import { AdjectiveComponent } from './adjective.component';
+
+const indexToUse = 3;
+const adjectiveToUse = 'awesome';
+const numberOfAdjectivesToUse = 6;
 
 describe('AdjectiveComponent', () => {
   let component: AdjectiveComponent;
@@ -8,14 +12,16 @@ describe('AdjectiveComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AdjectiveComponent ]
-    })
-    .compileComponents();
+      declarations: [AdjectiveComponent, InteractiveWordComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AdjectiveComponent);
     component = fixture.componentInstance;
+    component.index = indexToUse;
+    component.adjective = adjectiveToUse;
+    component.numberOfAdjectives = numberOfAdjectivesToUse;
     fixture.detectChanges();
   });
 
